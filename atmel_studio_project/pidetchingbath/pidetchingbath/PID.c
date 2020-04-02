6f38b366befb603737899d9ceb7e687880d7c2ee
FabianFriederichs <fabian.friederichs@gmail.com>
2020-04-02 12:16:10 +0200

    Update PID.c

--- atmel_studio_project/pidetchingbath/pidetchingbath/PID.c
+++ atmel_studio_project/pidetchingbath/pidetchingbath/PID.c
@@ -44,16 +44,16 @@ float pid_step(pid_state_t* state, float process_value, float set_value)
 	float error = set_value - process_value;
 	// proportional term
 	float output = state->offset + state->Kp * error;
-	// derivative term (instead of d/de use -d/dPV to get rid of set point spikes)
-	float d_dPV = ((process_value - state->old_process_value) / PID_DELTA_T);
+	// derivative term (instead of de/dt use -dPV/dt to get rid of set point spikes)
+	float dPV_dt = ((process_value - state->old_process_value) / PID_DELTA_T);
 	// second order exponential smoothing
-	state->smd1 = (1.0 - state->smoothing_factor) * d_dPV + state->smoothing_factor * state->smd1;
+	state->smd1 = (1.0 - state->smoothing_factor) * dPV_dt + state->smoothing_factor * state->smd1;
 	state->smd2 = (1.0 - state->smoothing_factor) * state->smd1 + (1.0 - state->smoothing_factor) * state->smd2;
 	// calculate d term contribution
 	output -= state->Kp * state->Td * state->smd2;
 	state->old_process_value = process_value;
 	// integral term
-	// integrate and clamp error signal; dynamic clamping! (and additionally scale the usable integrator range with i_clamp e[0, 1] to lessen the integrator overshoot for large delays)
+	// integrate and clamp error signal; dynamic clamping! (and additionally scale the usable integrator range with i_clamp e[0, 1] to reduce the integrator overshoot for large delays)
 	float i_max = fmax(state->control_max - output, 0.0) * state->i_clamp;
 	float i_min = fmin(state->control_min - output, 0.0) * state->i_clamp;
 	state->integrator = fmax(fmin(state->integrator + (state->Kp / fmax(state->Ti, 1.0 / F_CPU)) * error * PID_DELTA_T, i_max), i_min);
