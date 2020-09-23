ae5535633f082c470f1d34a8b6f346c1a4a108c0
Fabian Friederichs <fabian.friederichs@gmail.com>
2020-09-23 09:03:19 +0200

    Config stuff

--- atmel_studio_project/pidetchingbath/pidetchingbath/config.h
+++ atmel_studio_project/pidetchingbath/pidetchingbath/config.h
@@ -84,13 +84,13 @@
 // if heater is operating at full duty cycle, some temperature change is expected after some time interval.
 // if the temperature change after that time interval is smaller than the expected change, THERMAL_RUNAWAY_ERROR is triggered.
 #define HEATER_PROBE0_TR_PROTECTION_EXPECTED_TEMP_CHANGE 5.0 // expected temp change for probe 0
-#define HEATER_PROBE1_TR_PROTECTION_EXPECTED_TEMP_CHANGE 2.0 // expected temp change for probe 1
+#define HEATER_PROBE1_TR_PROTECTION_EXPECTED_TEMP_CHANGE 1.0 // expected temp change for probe 1
 //#define HEATER_PROBE2_EXPECTED_TEMP_CHANGE 1.0 //
 //#define HEATER_PROBE3_EXPECTED_TEMP_CHANGE 1.0 //
 
 // time intervals for thermal runway protection (time in seconds)
-#define HEATER_PROBE0_TR_PROTECTION_INTERVAL 15 // at full heater dc, the heater mat thermistor should read at least 5 degrees temp change within 15 seconds
-#define HEATER_PROBE1_TR_PROTECTION_INTERVAL (5 * 60) // at full heater dc, the bath thermistor should read at least 2 degrees temp change within 5 minutes
+#define HEATER_PROBE0_TR_PROTECTION_INTERVAL 60 // at full heater dc, the heater mat thermistor should read at least 5 degrees temp change within 60 seconds
+#define HEATER_PROBE1_TR_PROTECTION_INTERVAL (5 * 60) // at full heater dc, the bath thermistor should read at least 1 degrees temp change within 5 minutes
 //#define HEATER_PROBE2_TR_PROTECTION_INTERVAL 20
 //#define HEATER_PROBE3_TR_PROTECTION_INTERVAL 20
 
